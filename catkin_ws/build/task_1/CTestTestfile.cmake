87ec4f0a66c2fa99b54d17520e61ad45a4528190
amancodeblast <sharma.aman7698@gmail.com>
2020-09-18 15:39:06 +0530

    building for task1

---
+++ catkin_ws/build/task_1/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/aman/ros_projects/catkin_ws/src/task_1
+# Build directory: /home/aman/ros_projects/catkin_ws/build/task_1
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
